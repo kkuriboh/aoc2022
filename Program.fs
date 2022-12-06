7e06688d00ea8f60ac677bea157381104d738e84
Augusto Pieper <augustoopieper@gmail.com>
2022-12-05 22:24:01 -0300

    day4

--- Program.fs
+++ Program.fs
@@ -3,11 +3,13 @@
     let main argv =
         // printfn "%A" argv
 
-        printfn "day1: "
+        printfn "day1"
         Day1.Res.print_res
-        printfn "day2: "
+        printfn "day2"
         Day2.Res.print_res
-        printfn "day3: "
+        printfn "day2"
         Day3.Res.print_res
+        printfn "day4"
+        Day4.Res.print_res
 
         0
---
+++ day4.fs
@@ -0,0 +1,29 @@
+namespace Day4
+
+module Res =
+    open Utils.Helpers
+    let file = load_file "inputs/day4.txt"
+
+    let parse_file =
+        file
+        |> Array.map (fun line -> line.Split [| ',' |])
+        |> Array.map (fun line ->
+            let first = line[ 0 ].Split [| '-' |]
+            let second = line[ 1 ].Split [| '-' |]
+            (set { first[0] |> uint32 .. first[1] |> uint32 }, set { second[0] |> uint32 .. second[1] |> uint32 }))
+
+    let res1 =
+        parse_file
+        |> Array.filter (fun (left, right) ->
+            let intersection = Set.intersect left right |> Set.toArray
+            intersection = (left |> Set.toArray) || intersection = (right |> Set.toArray))
+        |> Array.length
+
+    let res2 =
+        parse_file
+        |> Array.filter (fun (left, right) ->
+            let intersection = Set.intersect left right |> Set.toArray
+            intersection.Length <> 0)
+        |> Array.length
+
+    let print_res = printfn "p1: %d\np2: %d" res1 res2
